71a30ee54695c0dfc013c99bf03ff4c294b81833
Bruno Peres <bruno_16515@hotmail.com>
2022-10-17 17:01:27 -0300

    :rocket: Pequenas refatorações

--- MaterClass - Manipulando a DOM/main.js
+++ MaterClass - Manipulando a DOM/main.js
@@ -5,32 +5,14 @@ function playNote(event) {
 
   const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`);
 
-  const cantFoundAnyKey = !key;
-
-  if (cantFoundAnyKey) {
-    return;
-  }
+  if (!key) return;
 
   addPlayingClass(key);
   playAudio(audioKeyCode);
 }
 
-function addPlayingClass(key) {
-  key.classList.add("playing");
-}
-
-function getKeyCode(event) {
-  const isKeyboard = event.type === "keydown";
-
-  let keyCode;
-
-  if (isKeyboard) {
-    keyCode = event.keyCode;
-  } else {
-    keyCode = event.target.dataset.key;
-  }
-
-  return keyCode;
+function getKeyCode(e) {
+  return e.type === "keydown" ? e.keyCode : e.target.dataset.key;
 }
 
 function playAudio(audioKeyCode) {
@@ -39,17 +21,19 @@ function playAudio(audioKeyCode) {
   audio.play();
 }
 
+function addPlayingClass(key) {
+  key.classList.add("playing");
+}
+
 function removePlayingClass(event) {
   event.target.classList.remove("playing");
 }
 
 function registerEvents() {
   keys.forEach(key => {
-    // console.log(key.dataset.note);
     key.addEventListener("click", playNote);
     key.addEventListener("transitionend", removePlayingClass);
   });
-
   window.addEventListener("keydown", playNote);
 }
 
